cb954772eff4d26f09025656230a6885d07b94bf
Quentin Cruz <napu0515@gmail.com>
2021-05-13 22:45:12 -0700

    lab04

--- src/modal.js
+++ src/modal.js
@@ -3,17 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-class hornedBeastModal extends React.component {
+class HBModal extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+  }
+
+
+render() { 
+  console.log(this.props)
+  return (
+    <div id="Modal">
+      <Modal show={this.props.newModal} onHide ={this.props.closeModal} style={{width: '50%'}}>
+        <Modal.Title>{this.props.title}</Modal.Title>
+        <Modal.Body>
+          <img src={this.props.src} alt ={this.props.alt} title={this.props.title}/>
+          <p>{this.props.description} </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant= "success" onClick={this.props.closeModal}>
+              Press to Leave {this.props.title}
+          </Button>
+        </Modal.Footer>
+        </Modal>
+    </div>
+  )
   }
 }
 
-render() {
-  return (
-    <div id="modal">
-      <Modal show={this.props,activeModal} onHide ={this.props.closeModal}>
-        <Modal.Title
-    </div>
-  )
-}
+export default HBModal;
